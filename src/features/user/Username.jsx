fa5e1b06327b7c8f98678204e533d94eec775973
devfritz <devfritzcode@gmail.com>
2023-11-20 13:39:32 -0500

    slice userName

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
 function Username() {
-  return <p className="text-slate-800 hidden md:block">Gerlin</p>;
+  const userName = useSelector((state) => state.user.userName);
+  console.log(userName);
+  return <p className="text-slate-800 hidden md:block">{userName}</p>;
 }
 
 export default Username;
